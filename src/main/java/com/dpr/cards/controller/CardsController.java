4cb0814be0817585128a0018f7b74bb4bd1ec04a
DeepaRajndr <133270634+DeepaRajndr@users.noreply.github.com>
2023-07-05 08:50:42 -0400

    Added Actuator and Refresh Scope

--- src/main/java/com/dpr/cards/controller/CardsController.java
+++ src/main/java/com/dpr/cards/controller/CardsController.java
@@ -3,24 +3,29 @@ package com.dpr.cards.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dpr.cards.config.CardsServiceConfig;
 import com.dpr.cards.model.Cards;
 import com.dpr.cards.model.Customer;
+import com.dpr.cards.model.Properties;
 import com.dpr.cards.repository.CardsRepository;
 
-/**
- * @author Eazy Bytes
- *
- */
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @RestController
 public class CardsController {
 
     @Autowired
     private CardsRepository cardsRepository;
+
+    @Autowired
+    CardsServiceConfig cardsConfig;
 
     @PostMapping("/myCards")
     public List<Cards> getCardDetails(@RequestBody Customer customer) {
@@ -31,6 +36,15 @@ public class CardsController {
             return null;
         }
 
+    }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+                cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 
 }
