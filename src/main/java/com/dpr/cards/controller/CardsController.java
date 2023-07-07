939274099c0cece1c5175b3438c64831fa7c839b
DeepaRajndr <133270634+DeepaRajndr@users.noreply.github.com>
2023-07-06 21:18:29 -0400

    Addted the TraceID update in Headers logic

--- src/main/java/com/dpr/cards/controller/CardsController.java
+++ src/main/java/com/dpr/cards/controller/CardsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dpr.cards.config.CardsServiceConfig;
@@ -28,7 +29,8 @@ public class CardsController {
     CardsServiceConfig cardsConfig;
 
     @PostMapping("/myCards")
-    public List<Cards> getCardDetails(@RequestBody Customer customer) {
+    public List<Cards> getCardDetails(@RequestHeader("dpr-correlation-id") String correlationid,
+            @RequestBody Customer customer) {
         List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
         if (cards != null) {
             return cards;
